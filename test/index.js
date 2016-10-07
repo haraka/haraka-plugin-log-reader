797559491b8236e0f9edc7477dd0baa3aa231fa9
Matt Simerson <matt@tnpi.net>
2016-10-06 23:07:01 -0700

    packaging updates (#5)

--- test/index.js
+++ test/index.js
@@ -9,58 +9,58 @@ var assert   = require('assert');
 var fixtures = require('haraka-test-fixtures');
 
 describe('register', function () {
-    var attach = new fixtures.plugin('index');
+  var attach = new fixtures.plugin('index');
 
-    it('is a function', function (done) {
-        assert.equal('function', typeof attach.register);
-        done();
-    });
+  it('is a function', function (done) {
+    assert.equal('function', typeof attach.register);
+    done();
+  });
 
-    it('runs', function (done) {
-        attach.register();
+  it('runs', function (done) {
+    attach.register();
         // console.log(attach.cfg);
-        done();
-    });
+    done();
+  });
 
-    it('loads log.reader.ini', function (done) {
-        attach.register();
-        assert.deepEqual(attach.cfg, {
-            main: {},
-            log: {
-                file: '/var/log/haraka.log'
-            }
-        });
-        done();
+  it('loads log.reader.ini', function (done) {
+    attach.register();
+    assert.deepEqual(attach.cfg, {
+      main: {},
+      log: {
+        file: '/var/log/haraka.log'
+      }
     });
+    done();
+  });
 
-    it('loads karma.ini', function (done) {
-        attach.register();
-        assert.equal(attach.karma_cfg.tarpit.delay, 0);
-        done();
-    });
+  it('loads karma.ini', function (done) {
+    attach.register();
+    assert.equal(attach.karma_cfg.tarpit.delay, 0);
+    done();
+  });
 });
 
 describe('log.reader.ini', function () {
-    var reader = new fixtures.plugin('index');
+  var reader = new fixtures.plugin('index');
 
-    it('has a log section', function (done) {
-        reader.register();
-        assert.ok(reader.cfg.log.file);
-        done();
-    });
+  it('has a log section', function (done) {
+    reader.register();
+    assert.ok(reader.cfg.log.file);
+    done();
+  });
 });
 
 // the subsequent functions require the express res/req
 // those could be mocked up, along with some sample log files
 
 describe('get_logs', function() {
-    it.skip('reads entries from a log file', function (done) {
-        done();
-    })
+  it.skip('reads entries from a log file', function (done) {
+    done();
+  })
 });
 
 describe('get_rules', function() {
-    it.skip('returns rules section from karma.ini', function (done) {
-        done();
-    })
+  it.skip('returns rules section from karma.ini', function (done) {
+    done();
+  })
 });
