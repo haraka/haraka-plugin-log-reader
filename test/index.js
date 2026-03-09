2f6806ccc7de9a4c188158626cc26ca87f2ea07c
Larry Nedry <lnedry@users.noreply.github.com>
2026-03-09 12:11:35 -0400

    Update tests to use done callback for async handling

--- test/index.js
+++ test/index.js
@@ -7,7 +7,7 @@ const path = require('node:path')
 
 const fixtures = require('haraka-test-fixtures')
 
-beforeEach(() => {
+beforeEach((done) => {
   this.reader = new fixtures.plugin('index')
 
   // Conditionally inject for coverage tracking
@@ -15,19 +15,23 @@ beforeEach(() => {
     const plugin_module = require('../index.js')
     Object.assign(this.reader, plugin_module)
   }
+
+  done()
 })
 
 describe('register', () => {
-  it('is a function', () => {
+  it('is a function', (done) => {
     assert.equal('function', typeof this.reader.register)
+    done()
   })
 
-  it('runs', () => {
+  it('runs', (done) => {
     this.reader.register()
     // console.log(reader.cfg)
+    done()
   })
 
-  it('loads log.reader.ini', () => {
+  it('loads log.reader.ini', (done) => {
     this.reader.register()
     assert.deepEqual(this.reader.cfg, {
       main: {},
@@ -35,32 +39,36 @@ describe('register', () => {
         file: '/var/log/haraka.log',
       },
     })
+    done()
   })
 
-  it('loads karma.ini', () => {
+  it('loads karma.ini', (done) => {
     this.reader.register()
     this.reader.config = this.reader.config.module_config(path.resolve('test'))
     this.reader.load_karma_ini()
     assert.equal(this.reader.karma_cfg.tarpit.delay, 0)
+    done()
   })
 })
 
 describe('log.reader.ini', () => {
-  it('has a log section', () => {
+  it('has a log section', (done) => {
     this.reader.register()
     assert.ok(this.reader.cfg.log.file)
+    done()
   })
 })
 
 describe('grepWithShell', () => {
-  beforeEach(() => {
+  beforeEach((done) => {
     this.reader = new fixtures.plugin('index')
     this.reader.register()
     this.reader.config = this.reader.config.module_config(path.resolve('test'))
     this.reader.load_karma_ini()
+    done()
   })
 
-  it('reads matching connection entries from a log file', () => {
+  it('reads matching connection entries from a log file', (done) => {
     const logfile = path.join('test', 'fixtures', 'haraka.log')
     this.reader.grepWithShell(
       logfile,
@@ -69,11 +77,12 @@ describe('grepWithShell', () => {
         assert.ifError(err)
         // console.log(r);
         assert.equal(r.split('\n').length - 1, 36)
+        done()
       },
     )
   })
 
-  it('reads matching transaction entries from a log file', () => {
+  it('reads matching transaction entries from a log file', (done) => {
     const logfile = path.join('test', 'fixtures', 'haraka.log')
     this.reader.grepWithShell(
       logfile,
@@ -82,11 +91,12 @@ describe('grepWithShell', () => {
         assert.ifError(err)
         // console.log(r);
         assert.equal(r.split('\n').length - 1, 36)
+        done()
       },
     )
   })
 
-  it('formats matching entries as HTML', () => {
+  it('formats matching entries as HTML', (done) => {
     const uuid = '3E6A027F-8307-4DA4-B105-2A39EC4B58D4.1'
     const logfile = path.join('test', 'fixtures', 'haraka.log')
     this.reader.grepWithShell(logfile, uuid, (err, r) => {
@@ -95,6 +105,7 @@ describe('grepWithShell', () => {
         // console.log(html);
         assert.ok(/^<html>/.test(html))
         assert.ok(/<\/html>$/.test(html))
+        done()
       })
     })
   })
@@ -109,7 +120,7 @@ describe('asHtml', () => {
     done()
   })
 
-  it('formats a block of log lines for HTML presentation', () => {
+  it('formats a block of log lines for HTML presentation', (done) => {
     const uuid = '9613CD00-7145-4ABC-8CA8-79CD9E39BB4F'
     const logfile = path.join('test', 'fixtures', 'haraka.log')
     this.reader.grepWithShell(logfile, uuid, (err, r) => {
@@ -117,6 +128,7 @@ describe('asHtml', () => {
         assert.ok(/^<html>/.test(html))
         assert.ok(/<\/html>/.test(html))
         // console.log(html);
+        done()
       })
     })
   })
@@ -126,5 +138,7 @@ describe('asHtml', () => {
 // those could be mocked up, along with some sample log files
 
 describe('get_rules', () => {
-  it.skip('returns rules section from karma.ini', () => {})
+  it.skip('returns rules section from karma.ini', (done) => {
+    done()
+  })
 })
